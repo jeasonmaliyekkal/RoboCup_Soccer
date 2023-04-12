de120b3c073df0570b8353716dd320b733d25b02
thelastsayian <123329017+thelastsayian@users.noreply.github.com>
2023-04-12 12:56:23 +0100

    Update tackle.m

--- Game1.8/tackle.m
+++ Game1.8/tackle.m
@@ -1,9 +1,11 @@
 function index = tackle(aggressorPos, positionMatrix)
-
+    
+    tackleArray = zeros(1,4);
         for i = 1:4
-            tackleArray = norm(aggressorPos-positionMatrix(i,:));
+            tackleArray(i) = norm(aggressorPos-positionMatrix(i,:));
  
         end
+
         [~ ,index] = min(tackleArray);
         
         
